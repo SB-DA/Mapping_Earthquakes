b00f231e90a3cec45f5cb15a908fd79efd2c586b
Shruti Bangera <bangerashruti@yahoo.com>
2021-05-20 12:37:23 -0400

    Update logic.js

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -10,7 +10,10 @@ let cityData = cities;
 // Loop through the cities array and create one marker for each city.
 cityData.forEach(function(city) {
   console.log(city)
-  L.marker(city.location)
+  L.circleMarker(city.location, {
+    radius: city.population/100000,
+    color: "orange"
+  });
   .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population + "</h3>")
  .addTo(map);
 });
